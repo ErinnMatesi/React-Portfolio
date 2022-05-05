3c0b4d1eb7139136966c5bbe15a32867a85a0810
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-05-04 19:13:42 -0700

    component base structure created with imports to App.js. Footer html pulled, will need styling updates

---
+++ reactportfolio/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="container">
+      <footer className="footer">
+        <ul className="contactList"> 
+          <li><a href="https://github.com/ErinnMatesi" target="_blank">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/ematesi/" target="_blank">LinkedIn</a></li>
+          <li><a href="https://twitter.com/thedoublenn" target="_blank">Twitter</a></li>  
+        </ul>
+      </footer>
+    </div>
+  );
+};
