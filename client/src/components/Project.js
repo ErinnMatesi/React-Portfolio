88818a29d5b68e9b456f0e66585c0de90de62d82
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-05-14 11:57:38 -0700

    project card styling updated with media queries

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -3,11 +3,13 @@ import React from 'react';
 export default function Project({ project }) {
   return (
     <section className="projectCard">
+      <h4><b>{project.title}</b></h4>
       <img className="projectImg" src={project.image} alt="screenshot of SproutSpy website" />
       <section className="projectLinks">
-        <h4><b>{project.name}</b></h4>
         <p>{project.description}</p>
+        <br></br>
         <a href={project.deployedLink} target="_blank">Deployed Site</a>
+        <br></br>
         <a href={project.githubLink} target="_blank">GitHub Repository</a>
       </section>
     </section>
