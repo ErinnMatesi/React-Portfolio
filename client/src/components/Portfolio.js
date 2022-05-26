f9fb71fdcaabee7ff067409ee0bc67305c55b27d
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-05-25 22:22:10 -0700

    updated portfolio cards and styling

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -4,37 +4,58 @@ import Project from './Project';
 export default function Portfolio() {
   const projectsArray = [
     {
-      title: "Sprout Spy",
-      description: "A web application that allows users to find their agricultural zone and which plants can be grow locally.",
+      title: "Tech Blog",
+      description: "A fullstack blog app that allows user to login to create and delete posts and comments.",
+      image: "./images/blog.PNG",
+      tech: "HTML - CSS - Bootstrap - Javascript - Express - Node - MySQL - Sequelize - Heroku - JawsDB",
+      deployedLink: "https://erinnstechblog.herokuapp.com/",
+      githubLink: "https://github.com/ErinnMatesi/Tech-Blog"
+    },
+    {
+      title: "Sprout Spy 2.0",
+      description: "Improved version of our gardening app, designed to get the beginner gardener started. Now including a database, user login and CRUD methods on the database.",
       image: "./images/SproutSpy.jpg",
+      tech: "HTML - CSS - Bootstrap - Javascript - jQuery - Google Maps API - Google Places API - Node - Express - Sequelize - MySQL - Azure - Handlebars - Heroku",
       deployedLink: "https://fast-cliffs-81659.herokuapp.com/",
       githubLink: "https://github.com/got-git-group/SproutSpy"
+    },
+      {
+      title: "Social Network Backend",
+      description: "Backend setup for a social network platform that allows the creation of thoughts that can receive reactions. CRUD methods on the MongoDB database. *Deployed link is a video walkthrough of API calls",
+      image: "./images/postmanScreenshot.PNG",
+      tech: "Node - Express - MongoDB - Mongoose - Moment",
+      deployedLink: "https://drive.google.com/file/d/1OKsS9zu_hSNmyFBGXNmZNGjauzQYKjxv/view",
+      githubLink: "https://github.com/ErinnMatesi/Social-Network-API"
     },
     {
       title: "Weather Dashboard",
       description: "A simple dashboard that takes a city input and displays the current weather conditions and a 5-day forecast.",
       image: "./images/WeatherDashboard.PNG",
+      tech: "HTML - CSS - Javascript - Bootstrap - jQuery - Moment - OpenWeatherMap API",
       deployedLink: "https://erinnmatesi.github.io/Weather-Dashboard/",
       githubLink: "https://github.com/ErinnMatesi/Weather-Dashboard"
     },
     {
       title: "Javascript Quiz",
-      description: "A quiz about Javascript built by Javascript!",
-      image: "./images/JSQuiz.PNG",
+      description: "A quiz about Javascript built by Javascript! Track your high score in local storage, wrong answers reduce your time!",
+      image: "./images/quizGrab.PNG",
+      tech: "HTML - CSS - Javascript",
       deployedLink: "https://erinnmatesi.github.io/Javascript-Quiz/",
       githubLink: "https://github.com/ErinnMatesi/Javascript-Quiz"
     },
-    {
-      title: "CSS Cheatsheet",
-      description: "Quick reference code blocks for CSS.",
-      image: "./images/Cheatsheet.PNG",
-      deployedLink: "https://erinnmatesi.github.io/CSS-CheatSheet/",
-      githubLink: "https://github.com/ErinnMatesi/CSS-CheatSheet"
-    },
+    // {
+    //   title: "CSS Cheatsheet",
+    //   description: "Quick reference code blocks for CSS.",
+    //   image: "./images/Cheatsheet.PNG",
+    //   tech: "",
+    //   deployedLink: "https://erinnmatesi.github.io/CSS-CheatSheet/",
+    //   githubLink: "https://github.com/ErinnMatesi/CSS-CheatSheet"
+    // },
     // {
     //   title: "",
     //   description: "",
     //   image: "",
+    //   tech: "",
     //   deployedLink: "",
     //   githubLink: ""
     // },
--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -5,12 +5,15 @@ export default function Project({ project }) {
     <section className="projectCard">
       <h4><b>{project.title}</b></h4>
       <img className="projectImg" src={project.image} alt="screenshot of SproutSpy website" />
-      <section className="projectLinks">
-        <p>{project.description}</p>
+      <section className="projectDetails">
+        <p id="projDescrip">{project.description}</p>
         <br></br>
-        <a href={project.deployedLink} target="_blank">Deployed Site</a>
+        <h5 id="techUsed">Technologies Used</h5>
+        <p id="techs">{project.tech}</p>
         <br></br>
-        <a href={project.githubLink} target="_blank">GitHub Repository</a>
+        <a class="projLinks" href={project.deployedLink} target="_blank">Deployed Site</a>
+        <br></br>
+        <a class="projLinks" href={project.githubLink} target="_blank">GitHub Repository</a>
       </section>
     </section>
   )
